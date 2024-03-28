71fcb194b302eb36b16cf48ee632e99acbbc528c
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 11:44:46 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -15,10 +15,10 @@ st.write(dt.head(10))
 st.subheader("สถิติข้อมูลดอกไม้ iris")
 st.write("ผลรวม")
 cl1,cl2,cl3,cl4 = st.columns(4)
-cl1.write(dt['sepal.length'].sum)
-cl2.write(dt['sepal.width'].sum)
-cl3.write(dt['petal.length'].sum)
-cl4.write(dt['petal.width'].sum)
+cl1.write(dt['sepal.length'].sum())
+cl2.write(dt['sepal.width'].sum())
+cl3.write(dt['petal.length'].sum())
+cl4.write(dt['petal.width'].sum())
 #Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
 #      'variety'],
 
