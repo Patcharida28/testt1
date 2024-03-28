444c75368d9e7821e8db4b3b455b2aecc6753807
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 11:25:07 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Website Developing using Python")
-st.header("Website Developing using Python")
+st.header("🌶️Website Developing using Python🌶️")
 st.subheader("🍔Patcharida Choomchoo🍔")
 st.image('patch.jpg')
