14232ef0a47f8b6a51127059561484dd8d34d9f3
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 11:35:08 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+
 st.title("Website Developing using Python")
 st.header("🌶️Website Developing using Python🌶️")
+
 st.subheader("🍔Patcharida Choomchoo🍔")
 st.image('patch.jpg')
 
-dt=pd.read_csv('/content/iris.csv')
+dt=pd.read_csv('/data/iris.csv')
 st.write(dt.head('10'))
