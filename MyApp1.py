5c23dd7f346f3437f0d7673d2bc02b6f182f2b33
Patcharida28 <patcharidachoomchoo@gmail.com>
2024-03-28 11:43:42 +0700

    .

--- MyApp1.py
+++ MyApp1.py
@@ -7,5 +7,21 @@ st.header("🌶️Website Developing using Python🌶️")
 st.subheader("🍔Patcharida Choomchoo🍔")
 st.image('patch.jpg')
 
-dt=pd.read_csv('/data/iris.csv')
-st.write(dt.head('10'))
+dt=pd.read_csv('_/data/iris.csv')
+
+st.header("ข้อมูลดอกไม้ iris")
+st.write(dt.head(10))
+
+st.subheader("สถิติข้อมูลดอกไม้ iris")
+st.write("ผลรวม")
+cl1,cl2,cl3,cl4 = st.columns(4)
+cl1.write(dt['sepal.length'].sum)
+cl2.write(dt['sepal.width'].sum)
+cl3.write(dt['petal.length'].sum)
+cl4.write(dt['petal.width'].sum)
+#Index(['sepal.length', 'sepal.width', 'petal.length', 'petal.width',
+#      'variety'],
+
+st.write("ค่าเฉลี่ย")
+st.write("ค่ามากที่สุด")
+st.write("ค่าน้อยที่สุด")
